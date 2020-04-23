13326bacb80c9d75539897d10846c9c5aa371f7a
dmlzj <284832506@qq.com>
2020-04-23 14:50:57 +0800

    打包模式错误

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@
  * @Github: https://github.com/dmlzj
  * @Email: 284832506@qq.com
  * @Date: 2020-01-10 15:32:32
- * @LastEditors  : dmlzj
- * @LastEditTime : 2020-01-10 15:36:51
+ * @LastEditors: dmlzj
+ * @LastEditTime: 2020-04-23 14:45:14
  * @如果有bug，那肯定不是我的锅，嘤嘤嘤
  */
 const path = require('path');
@@ -14,6 +14,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
+    libraryTarget: 'commonjs2',
     filename: 'vlf.js',
     path: path.resolve(__dirname, 'dist')
   }
